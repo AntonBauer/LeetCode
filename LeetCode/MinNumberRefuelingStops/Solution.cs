75cb7b73ba0895142e532080bb4923aacfa6aed0
Anton Bauer <abton.bauer@gmail.com>
2022-08-20 21:39:19 +0200

    Check solution with nunit

---
+++ LeetCode/MinNumberRefuelingStops/Solution.cs
@@ -0,0 +1,58 @@
+﻿namespace MinNumberRefuelingStops;
+
+internal static class Solution
+{
+    public static int MinRefuelStops(int target, int startFuel, int[][] stations) =>
+        CalculateMinRefuelStops(0, startFuel, AddTargetAsEmptyStation(target, stations), 0);
+
+    private static int CalculateMinRefuelStops(int currentPosition, int currentFuel, int[][] stations, int currentStops)
+    {
+        var furthestReachableIndex = FurthestReachableIndex(stations, currentPosition, currentFuel);
+        var reachableStations =
+            furthestReachableIndex < 0 ? Array.Empty<int[]>() : stations[..(furthestReachableIndex + 1)];
+
+        if (!reachableStations.Any())
+            return -1;
+        if (reachableStations.Length == stations.Length)
+            return currentStops;
+
+        for (var i = reachableStations.Length - 1; i >= 0; i--)
+        {
+            var currentStation = reachableStations[i];
+            var newPosition = currentStation[0];
+            var newFuel = currentFuel - (currentStation[0] - currentPosition) + currentStation[1];
+            var newStations = stations[(furthestReachableIndex + 1)..];
+
+            var stops = CalculateMinRefuelStops(newPosition, newFuel, newStations, currentStops + 1);
+            if (stops >= 0)
+                return stops;
+        }
+
+        return -1;
+    }
+
+    private static int[][] AddTargetAsEmptyStation(int target, int[][] stations)
+    {
+        var includeEnd = new int[stations.Length + 1][];
+        stations.CopyTo(includeEnd, 0);
+        includeEnd[^1] = new[] { target, 0 };
+
+        return includeEnd;
+    }
+
+    private static int FurthestReachableIndex(int[][] stations, int currentPosition, int currentFuel)
+    {
+        for (var i = stations.Length - 1; i >= 0; i--)
+        {
+            if (!IsReachable(stations[i], currentPosition, currentFuel))
+                continue;
+
+            return i;
+        }
+
+        return -1;
+    }
+
+    private static bool IsReachable(int[] station, int currentPosition, int currentFuel) =>
+        station[0] - currentPosition <= currentFuel;
+}
---
+++ LeetCode/MinNumberRefuelingStops/Tests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace MinNumberRefuelingStops;
+
+[TestFixture]
+public class Tests
+{
+    [Test]
+    [TestCaseSource(nameof(TestCases))]
+    public int TestRefuelingStops(int target, int startFuel, int[][] stations, int expected) =>
+        Solution.MinRefuelStops(target, startFuel, stations);
+
+    public static IEnumerable<TestCaseData> TestCases()
+    {
+        yield return new TestCaseData(1, 1, Array.Empty<int[]>()).Returns(1);
+        yield return new TestCaseData(100, 1, new[] { new[] { 10, 100 } }).Returns(-1);
+        yield return new TestCaseData(100, 10,
+            new[] { new[] { 10, 60 }, new[] { 20, 30 }, new[] { 30, 30 }, new[] { 60, 40 } }).Returns(2);
+        yield return new TestCaseData(100, 50, new[] { new[] { 25, 50 }, new[] { 50, 25 } }).Returns(1);
+    }
+}
