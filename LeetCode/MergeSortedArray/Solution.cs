4bdd7cecbdfba7ab2290fa1c9cc704fca580ffba
Anton Bauer <abton.bauer@gmail.com>
2022-08-21 21:01:09 +0200

    88. Merge sorted array WIP

---
+++ LeetCode/MergeSortedArray/Solution.cs
@@ -0,0 +1,31 @@
+﻿namespace MergeSortedArray;
+
+internal static class Solution
+{
+    public static void Merge(int[] nums1, int m, int[] nums2, int n)
+    {
+        var first = 0;
+        var second = 0;
+
+        while ( /*first < m &&*/ second < n)
+        {
+            if (nums2[second] < nums1[first] || nums1[first] == 0)
+            {
+                ShiftRight(nums1, first);
+                nums1[first] = nums2[second];
+                ++second;
+            }
+            else
+                ++first;
+        }
+    }
+
+    private static void ShiftRight(int[] array, int index)
+    {
+        for (var i = array.Length - 2; i > index; i--)
+        {
+            array[i] = array[i - 1];
+            array[i - 1] = 0;
+        }
+    }
+}
---
+++ LeetCode/MergeSortedArray/Tests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+
+namespace MergeSortedArray;
+
+[TestFixture]
+public class Tests
+{
+    [Test, TestCaseSource(nameof(TestCases))]
+    public void TestMerge(int[] nums1, int m, int[] nums2, int n, int[] expected)
+    {
+        // Arrange
+        // Act
+        Solution.Merge(nums1, m, nums2, n);
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(nums1, Is.EquivalentTo(expected));
+        });
+    }
+
+    public static IEnumerable<TestCaseData> TestCases()
+    {
+        yield return new TestCaseData(new[] { 1, 2, 3, 0, 0, 0 }, 3, new[] { 2, 5, 6 }, 3, new[] { 1, 2, 2, 3, 5, 6 }).SetName("01");
+        yield return new TestCaseData(new[] { 1 }, 1, Array.Empty<int>(), 0, new[] { 1 }).SetName("02");
+        yield return new TestCaseData(new[] { 0 }, 0, new[] { 1 }, 1, new[] { 1 }).SetName("03");
+        yield return new TestCaseData(new[] { 4, 5, 6, 0, 0, 0 }, 3, new[] { 1, 2, 3 }, 3, new[] { 1 ,2, 3, 4, 5, 6 }).SetName("04");
+    }
+}
