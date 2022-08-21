97c0db593345a5fc39ae480db3225f999eaa6532
Anton Bauer <abton.bauer@gmail.com>
2022-08-21 19:48:46 +0200

    1. Two sum

---
+++ LeetCode/TwoSum/Solution.cs
@@ -0,0 +1,21 @@
+﻿namespace TwoSum;
+
+internal static class Solution
+{
+    public static int[] TwoSum(int[] nums, int target)
+    {
+        var numbersMap = new Dictionary<int, int>();
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            var diff = target - nums[i];
+            if (numbersMap.ContainsKey(diff))
+                return new[] { numbersMap[diff], i };
+
+            if (!numbersMap.ContainsKey(nums[i]))
+                numbersMap.Add(nums[i], i);
+        }
+
+        return Array.Empty<int>();
+    }
+}
---
+++ LeetCode/TwoSum/Tests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace TwoSum;
+
+[TestFixture]
+public class Tests
+{
+    [Test, TestCaseSource(nameof(TestCases))]
+    public int[] TestTwoSum(int[] nums, int target) => Solution.TwoSum(nums, target);
+
+    public static IEnumerable<TestCaseData> TestCases()
+    {
+        yield return new TestCaseData(new[] { 2, 7, 11, 15 }, 9).Returns(new[] { 0, 1 });
+        yield return new TestCaseData(new[] { 3, 2, 4 }, 6).Returns(new[] { 1, 2 });
+        yield return new TestCaseData(new[] { 3, 3 }, 6).Returns(new[] { 0, 1 });
+    }
+}
