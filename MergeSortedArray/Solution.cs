606c835adbb41c1d6ab65b4bdb8847014a7c61ed
Anton Bauer <anton.bauer@finanzen.net>
2022-08-23 10:55:45 +0200

    88. Merge Sorted Array solved

--- MergeSortedArray/Solution.cs
+++ MergeSortedArray/Solution.cs
@@ -1,19 +1,22 @@
 ﻿namespace MergeSortedArray;
 
+// 88. Merge Sorted Array
 internal static class Solution
 {
     public static void Merge(int[] nums1, int m, int[] nums2, int n)
     {
         var first = 0;
         var second = 0;
+        var inserted = 0;
 
         while (second < n)
         {
-            if (nums2[second] < nums1[first] || nums1[first] == 0)
+            if (nums2[second] < nums1[first] || first >= m + inserted)
             {
                 ShiftRight(nums1, first);
                 nums1[first] = nums2[second];
                 ++second;
+                ++inserted;
             }
             else
                 ++first;
