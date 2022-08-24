3a6dfbbc6727b70344e28b0627237a40622198bf
Anton Bauer <anton.bauer@finanzen.net>
2022-08-24 09:29:00 +0200

    326. Power of Three solved

---
+++ PowerOfThree/Solution.cs
@@ -0,0 +1,13 @@
+﻿namespace PowerOfThree;
+
+// 326. Power of Three
+internal static class Solution
+{
+    public static bool IsPowerOfThree(int n)
+    {
+        var log = Math.Log(n, 3);
+        var rounded = Math.Round(log);
+
+        return Math.Abs(rounded - log) < 1e-10;
+    } 
+}
---
+++ PowerOfThree/Tests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace PowerOfThree;
+
+[TestFixture]
+public class Tests
+{
+    [Test, TestCaseSource(nameof(TestCases))]
+    public bool TestIsPowerOfThree(int n) => Solution.IsPowerOfThree(n);
+
+    public static IEnumerable<TestCaseData> TestCases()
+    {
+        yield return new TestCaseData(27).Returns(true);
+        yield return new TestCaseData(0).Returns(false);
+        yield return new TestCaseData(9).Returns(true);
+        yield return new TestCaseData(243).Returns(true);
+        yield return new TestCaseData(59049).Returns(true);
+    }
+}
