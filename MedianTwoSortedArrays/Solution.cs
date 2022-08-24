73c8c94901beb4cfdbf4e68ae308c6d2c20c7ac5
Anton Bauer <anton.bauer@finanzen.net>
2022-08-24 14:51:52 +0200

    4. Median of Two Sorted Arrays solved

---
+++ MedianTwoSortedArrays/Solution.cs
@@ -0,0 +1,61 @@
+﻿namespace MedianTwoSortedArrays;
+
+// 4. Median of Two Sorted Arrays
+internal static class Solution
+{
+    public static double FindMedianSortedArrays(int[] nums1, int[] nums2) => Median(MergeArrays(nums1, nums2));
+
+    private static int[] MergeArrays(int[] nums1, int[] nums2)
+    {
+        var result = new int[nums1.Length + nums2.Length];
+
+        var resultPosition = 0;
+        var firstPosition = 0;
+        var secondPosition = 0;
+
+        while (resultPosition < result.Length)
+        {
+            if (firstPosition == nums1.Length)
+            {
+                result[resultPosition] = nums2[secondPosition];
+                ++secondPosition;
+            }
+            else if (secondPosition == nums2.Length)
+            {
+                result[resultPosition] = nums1[firstPosition];
+                ++firstPosition;
+            }
+            else if (nums1[firstPosition] > nums2[secondPosition])
+            {
+                result[resultPosition] = nums2[secondPosition];
+                ++secondPosition;
+            }
+            else
+            {
+                result[resultPosition] = nums1[firstPosition];
+                ++firstPosition;
+            }
+
+            ++resultPosition;
+        }
+
+        return result;
+    }
+
+    private static double Median(int[] nums)
+    {
+        switch (nums.Length)
+        {
+            case 1:
+                return nums[0];
+            default:
+            {
+                var middle = nums.Length / 2;
+
+                return middle * 2 == nums.Length
+                    ? (nums[middle - 1] + nums[middle]) / 2d
+                    : nums[middle];
+            }
+        }
+    }
+}
---
+++ MedianTwoSortedArrays/Tests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace MedianTwoSortedArrays;
+
+public class Tests
+{
+    [Test, TestCaseSource(nameof(TestCases))]
+    public double TestFindMedianSortedArrays(int[] nums1, int[] nums2) => Solution.FindMedianSortedArrays(nums1, nums2);
+
+    public static IEnumerable<TestCaseData> TestCases()
+    {
+        yield return new TestCaseData(new[] { 1, 3 }, new[] { 2 }).Returns(2).SetName("01");
+        yield return new TestCaseData(new[] { 1, 2 }, new[] { 3, 4 }).Returns(2.5).SetName("02");
+        yield return new TestCaseData(Array.Empty<int>(), new[] { 1 }).Returns(1).SetName("03");
+        yield return new TestCaseData(new[] { 1, 3 }, new[] { 2, 7 }).Returns(2.5).SetName("04");
+        yield return new TestCaseData(Array.Empty<int>(), new[] { 2, 3 }).Returns(2.5).SetName("05");
+    }
+}
