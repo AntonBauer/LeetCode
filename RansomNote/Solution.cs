5078f89f040e9f51c8e7bd7f1b7819f778c50f34
Anton Bauer <anton.bauer@finanzen.net>
2022-08-24 10:24:40 +0200

    383. Ransom Note solved

---
+++ RansomNote/Solution.cs
@@ -0,0 +1,28 @@
+﻿namespace RansomNote;
+
+// 383. Ransom Note
+internal static class Solution
+{
+    public static bool CanConstruct(string ransomNote, string magazine)
+    {
+        var ransomMap = ConstructMap(ransomNote);
+        var magMap = ConstructMap(magazine);
+
+        return ransomMap.All(pair => magMap.ContainsKey(pair.Key) && magMap[pair.Key] >= pair.Value);
+    }
+
+    private static Dictionary<char, int> ConstructMap(string str)
+    {
+        var map = new Dictionary<char, int>();
+        
+        foreach (var letter in str)
+        {
+            if (!map.ContainsKey(letter))
+                map.Add(letter, 1);
+            else
+                map[letter]++;
+        }
+
+        return map;
+    }
+}
---
+++ RansomNote/Tests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace RansomNote;
+
+[TestFixture]
+public class Tests
+{
+    [Test, TestCaseSource(nameof(TestCases))]
+    public bool TestCanConstruct(string ransomNote, string magazine) => Solution.CanConstruct(ransomNote, magazine);
+    public static IEnumerable<TestCaseData> TestCases()
+    {
+        yield return new TestCaseData("a", "b").Returns(false).SetName("01");
+        yield return new TestCaseData("aa", "ab").Returns(false).SetName("02");
+        yield return new TestCaseData("aa", "aab").Returns(true).SetName("03");
+    }
+}
