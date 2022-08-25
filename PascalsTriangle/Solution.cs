b1969b240c11f87d4d52a511d311f3c7bf18210d
Anton Bauer <anton.bauer@finanzen.net>
2022-08-25 10:50:07 +0200

    // 118. Pascal's Triangle solved

---
+++ PascalsTriangle/Solution.cs
@@ -0,0 +1,40 @@
+﻿namespace PascalsTriangle;
+
+// 118. Pascal's Triangle
+internal static class Solution
+{
+    public static IList<IList<int>> Generate(int numRows)
+    {
+        var triangle = new IList<int>[numRows];
+        var currentRow = Array.Empty<int>();
+
+        for (var i = 0; i < numRows; i++)
+        {
+            var row = CreateRow(i, currentRow);
+            triangle[i] = row;
+            currentRow = row;
+        }
+        
+        return triangle;
+    }
+
+    private static int[] CreateRow(int rowNumber, int[] prevRow)
+    {
+        switch (rowNumber)
+        {
+            case 0:
+                return new[] { 1 };
+            case 1:
+                return new[] { 1, 1 };
+            default:
+                var row = new int[rowNumber + 1];
+                row[0] = 1;
+                row[^1] = 1;
+
+                for (var i = 1; i < row.Length - 1; i++)
+                    row[i] = prevRow[i - 1] + prevRow[i];
+                
+                return row;
+        }
+    }
+}
---
+++ PascalsTriangle/Tests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace PascalsTriangle;
+
+[TestFixture]
+public class Tests
+{
+    [Test, TestCaseSource(nameof(TestCases))]
+    public IList<IList<int>> TestGenerate(int numRows) => Solution.Generate(numRows);
+
+    public static IEnumerable<TestCaseData> TestCases()
+    {
+        yield return new TestCaseData(5).Returns(new[] { new[] { 1 }, new[] { 1, 1 }, new[] { 1, 2, 1 }, new[] { 1, 3, 3, 1 }, new[] { 1, 4, 6, 4, 1 } });
+        yield return new TestCaseData(1).Returns(new[] { new[] { 1 } });
+    }
+}
