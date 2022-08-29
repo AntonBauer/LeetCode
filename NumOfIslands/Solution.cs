e62dd9757b194530b935ebd3e4bdf9fc075339d1
Anton Bauer <abton.bauer@gmail.com>
2022-08-29 21:22:05 +0200

    200. Number of islands WIP

---
+++ NumOfIslands/Solution.cs
@@ -0,0 +1,76 @@
+﻿namespace NumOfIslands;
+
+// 200. Number of Islands
+internal static class Solution
+{
+    private record Position(int Row, int Column);
+
+    private static int _rows;
+    private static int _columns;
+    
+    private static void Init(char[][] grid)
+    {
+        _rows = grid.Length;
+        _columns = grid[0].Length;
+    }
+
+    public static int NumOfIslands(char[][] grid)
+    {
+        Init(grid);
+        
+        var flatMap = new int[_rows * _columns];
+
+        var lastIslandNum = 0;
+        Position islandPosition;
+
+        do
+        {
+            islandPosition = FindNextIsland(flatMap, grid);
+            if (islandPosition.Row == _rows && islandPosition.Column == _columns)
+                return lastIslandNum;
+
+            ++lastIslandNum;
+            ExploreIsland(islandPosition, grid, lastIslandNum, flatMap);
+        } while (islandPosition.Row < _rows && islandPosition.Column < _columns);
+
+        return lastIslandNum;
+    }
+    
+    private static Position FindNextIsland(int[] flatMap, char[][] map)
+    {
+        for(var i = 0; i < _rows; i++)
+        for (var j = 0; j < _columns; j++)
+        {
+            var pos = new Position(i, j);
+            if (map[i][j] == '1' && flatMap[ToFlat(pos)] == 0)
+                return pos;
+        }
+        
+        return new Position(_rows, _columns);
+    }
+
+    private static void ExploreIsland(Position start, char[][] map, int islandNum, int[] flatMap)
+    {
+        if (start.Row >= _rows && start.Column >= _columns)
+            return;
+        
+        if(map[start.Row][start.Column] == '0')
+            return;
+
+        var flatPosition = ToFlat(start);
+        if(flatMap[flatPosition] != 0)
+            return;
+        
+        flatMap[flatPosition] = islandNum;
+
+        // Explore right
+        if (start.Column < _columns)
+            ExploreIsland(start with { Column = start.Column + 1 }, map, islandNum, flatMap);
+        
+        // Explore down
+        if(start.Row < _rows)
+            ExploreIsland(start with {Row = start.Row + 1}, map, islandNum, flatMap);
+    }
+
+    private static int ToFlat(Position position) => position.Row * _columns + position.Column;
+}
---
+++ NumOfIslands/Tests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+
+namespace NumOfIslands;
+
+[TestFixture]
+public class Tests
+{
+    [Test, TestCaseSource(nameof(TestCases))]
+    public int TestNumOfIslands(List<char[]> grid) => Solution.NumOfIslands(grid.ToArray());
+
+    public static IEnumerable<TestCaseData> TestCases()
+    {
+        yield return new TestCaseData(new[]
+        {
+            new[] { '1', '1', '1', '1', '0' },
+            new[] { '1', '1', '0', '1', '0' },
+            new[] { '1', '1', '0', '0', '0' },
+            new[] { '0', '0', '0', '0', '0' }
+        }.ToList()).Returns(1).SetName("01");
+
+        yield return new TestCaseData(new[]
+        {
+            new[] { '1', '1', '0', '0', '0' },
+            new[] { '1', '1', '0', '0', '0' },
+            new[] { '0', '0', '1', '0', '0' },
+            new[] { '0', '0', '0', '1', '1' }
+        }.ToList()).Returns(3).SetName("02");
+    }
+}
