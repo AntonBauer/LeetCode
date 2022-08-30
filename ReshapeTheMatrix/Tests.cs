e4b482cc08cd0108c644873a3e3e45bdd5c960c5
Anton Bauer <anton.bauer@finanzen.net>
2022-08-30 07:07:18 +0200

    566. Reshape the Matrix solved

---
+++ ReshapeTheMatrix/Tests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace ReshapeTheMatrix;
+
+[TestFixture]
+public class Tests
+{
+    [Test, TestCaseSource(nameof(TestCases))]
+    public int[][] TestReshape(int[][] mat, int r, int c) => Solution.MatrixReshape(mat, r, c);
+
+    public static IEnumerable<TestCaseData> TestCases()
+    {
+        yield return new TestCaseData(new[] { new[] { 1, 2 }, new[] { 3, 4 } }, 1, 4).Returns(new[] { new[] { 1, 2, 3, 4 } }).SetName("01");
+        yield return new TestCaseData(new[] { new[] { 1, 2 }, new[] { 2, 4 } }, 2, 4).Returns(new[] { new[] { 1, 2 }, new[] { 2, 4 } }).SetName("02");
+        yield return new TestCaseData(new[] { new[] { 1, 2 }, new[] { 3, 4 } }, 4, 1).Returns(new[] { new[] { 1 }, new[] { 2 }, new[] { 3 }, new[] { 4 } }).SetName("03");
+    }
+}
