1a7901286698dd5a5be373f574ee15e21e81eefc
Anton Bauer <anton.bauer@finanzen.net>
2022-08-30 12:45:22 +0200

    48. Rotate Image solved

---
+++ RotateImage/Solution.cs
@@ -0,0 +1,25 @@
+﻿namespace RotateImage;
+
+// 48. Rotate Image
+internal static class Solution
+{
+    public static void Rotate(int[][] matrix)
+    {
+        Transpose(matrix);
+        ReverseRows(matrix);
+    }
+
+    private static void Transpose(int[][] matrix)
+    {
+        for (var i = 0; i < matrix.Length; i++)
+        for (var j = i + 1; j < matrix[0].Length; j++)
+            (matrix[i][j], matrix[j][i]) = (matrix[j][i], matrix[i][j]);
+    }
+
+    private static void ReverseRows(int[][] matrix)
+    {
+        for (var i = 0; i < matrix.Length; i++)
+        for (var j = 0; j < matrix.Length / 2; j++)
+            (matrix[i][j], matrix[i][matrix.Length - 1 - j]) = (matrix[i][matrix.Length - 1 - j], matrix[i][j]);
+    }
+}
