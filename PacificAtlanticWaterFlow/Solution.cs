f7fafb68cd321db17c4642e3bd586c8b313b0bbe
Anton Bauer <anton.bauer@finanzen.net>
2022-08-31 13:56:35 +0200

    // 417. Pacific Atlantic Water Flow WIP

--- PacificAtlanticWaterFlow/Solution.cs
+++ PacificAtlanticWaterFlow/Solution.cs
@@ -1,10 +1,80 @@
 ﻿namespace PacificAtlanticWaterFlow;
 
 // 417. Pacific Atlantic Water Flow
+internal record Position(int Row, int Col);
+
 internal static class Solution
 {
-    public static IList<IList<int>> PacificAtlantic(int[][] heights)
+    private enum ExitTo
     {
-        return default;
+        Pacific,
+        Atlantic
     }
+
+    public static int[][] PacificAtlantic(int[][] heights)
+    {
+        var exits = new Dictionary<Position, HashSet<ExitTo>>();
+
+        for (var i = 0; i < heights.Length; i++)
+        for (var j = 0; j < heights[0].Length; j++)
+            FindExits(new Position(i, j), heights, exits);
+
+        return exits.Where(e => e.Value.Count == 2).Select(e => new[] { e.Key.Row, e.Key.Col }).ToArray(); 
+    }
+
+    private static void FindExits(Position position, int[][] island, Dictionary<Position, HashSet<ExitTo>> exits)
+    {
+        if (exits.ContainsKey(position)) return;
+
+        exits.Add(position, new HashSet<ExitTo>());
+
+        var rows = island.Length;
+        var cols = island[0].Length;
+
+
+        if (position.IsDirectPacific())
+            exits[position].Add(ExitTo.Pacific);
+
+        if (position.IsDirectAtlantic(rows, cols))
+            exits[position].Add(ExitTo.Atlantic);
+
+        var currentHeight = island[position.Row][position.Col];
+        foreach (var neighbour in position.Neighbours(rows, cols).Where(n => n.IsLowerThan(currentHeight, island)))
+        {
+            if (!exits.ContainsKey(neighbour))
+                FindExits(neighbour, island, exits);
+
+            exits[position].UnionWith(exits[neighbour]);
+        }
+    }
+}
+
+internal static class Extensions
+{
+    public static bool IsDirectPacific(this Position pos) => pos.Row == 0 || pos.Col == 0;
+
+    public static bool IsDirectAtlantic(this Position pos, int rows, int cols) =>
+        pos.Row == rows - 1 || pos.Col == cols - 1;
+
+    public static IEnumerable<Position> Neighbours(this Position currentPosition, int rows, int columns)
+    {
+        // Top
+        if (currentPosition.Row > 0)
+            yield return currentPosition with { Row = currentPosition.Row - 1 };
+
+        // Right
+        if (currentPosition.Col < columns - 1)
+            yield return currentPosition with { Col = currentPosition.Col + 1 };
+
+        // Down
+        if (currentPosition.Row < rows - 1)
+            yield return currentPosition with { Row = currentPosition.Row + 1 };
+
+        // Left
+        if (currentPosition.Col > 0)
+            yield return currentPosition with { Col = currentPosition.Col - 1 };
+    }
+
+    public static bool IsLowerThan(this Position pos, int currentHeight, int[][] island) =>
+        island[pos.Row][pos.Col] <= currentHeight;
 }
--- PacificAtlanticWaterFlow/Tests.cs
+++ PacificAtlanticWaterFlow/Tests.cs
@@ -6,23 +6,40 @@ namespace PacificAtlanticWaterFlow;
 public class Tests
 {
     [Test, TestCaseSource(nameof(TestCases))]
-    public IList<IList<int>> TestPacificAtlantic(int[][] heights) => Solution.PacificAtlantic(heights);
+    public void TestPacificAtlantic(List<int[]> heights, List<int[]> expected)
+    {
+        // Act
+        var actual = Solution.PacificAtlantic(heights.ToArray());
+
+        // Assert
+        CollectionAssert.AreEquivalent(actual, expected);
+    }
 
     public static IEnumerable<TestCaseData> TestCases()
     {
         yield return new TestCaseData(new[]
-        {
-            new[] { 1, 2, 2, 3, 5 },
-            new[] { 3, 2, 3, 4, 4 },
-            new[] { 2, 4, 5, 3, 1 },
-            new[] { 6, 7, 1, 4, 5 },
-            new[] { 5, 1, 1, 2, 4 }
-        }).Returns(new[]
-        {
-            new[] { 0, 4 }, new[] { 1, 3 },
-            new[] { 1, 4 }, new[] { 2, 2 },
-            new[] { 3, 0 }, new[] { 3, 1 },
-            new[] { 4, 0 }
-        }).SetName("01");
+            {
+                new[] { 1, 2, 2, 3, 5 },
+                new[] { 3, 2, 3, 4, 4 },
+                new[] { 2, 4, 5, 3, 1 },
+                new[] { 6, 7, 1, 4, 5 },
+                new[] { 5, 1, 1, 2, 4 }
+            }.ToList(),
+            new[]
+            {
+                new[] { 0, 4 }, new[] { 1, 3 },
+                new[] { 1, 4 }, new[] { 2, 2 },
+                new[] { 3, 0 }, new[] { 3, 1 },
+                new[] { 4, 0 }
+            }.ToList()).SetName("01");
+
+        yield return new TestCaseData(new[]
+            {
+                new[] { 1 }
+            }.ToList(),
+            new[]
+            {
+                new[] { 0, 0 }
+            }.ToList()).SetName("02");
     }
 }
