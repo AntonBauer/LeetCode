a6ea8965ddc83eab196fdebd6de92a1d1d26ff49
Anton Bauer <anton.bauer@finanzen.net>
2022-09-01 09:54:36 +0200

    74. Search a 2D Matrix solved

---
+++ Search2dMatrix/Solution.cs
@@ -0,0 +1,22 @@
+﻿namespace Search2dMatrix;
+
+// 74. Search a 2D Matrix
+internal static class Solution
+{
+    public static bool SearchMatrix(int[][] matrix, int target)
+    {
+        if (matrix[0][0] > target)
+            return false;
+        if (matrix[^1][^1] < target)
+            return false;
+        
+        for (var i = 0; i < matrix.Length; i++)
+        {
+            var row = matrix[i];
+            if (row[0] <= target && row[^1] >= target)
+                return row.Contains(target);
+        }
+        
+        return false;
+    }
+}
---
+++ Search2dMatrix/Tests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+
+namespace Search2dMatrix;
+
+[TestFixture]
+public class Tests
+{
+    [Test, TestCaseSource(nameof(TestCases))]
+    public bool TestSearch(int[][] matrix, int target) => Solution.SearchMatrix(matrix, target);
+
+    public static IEnumerable<TestCaseData> TestCases()
+    {
+        yield return new TestCaseData(new[]
+        {
+            new[] { 1, 3, 5, 7 },
+            new[] { 10, 11, 16, 20 },
+            new[] { 23, 30, 34, 60 }
+        }, 3).Returns(true).SetName("01");
+
+        yield return new TestCaseData(new[]
+        {
+            new[] { 1, 3, 5, 7 },
+            new[] { 10, 11, 16, 20 },
+            new[] { 23, 30, 34, 60 }
+        }, 13).Returns(false).SetName("02");
+        
+        yield return new TestCaseData(new[]
+        {
+            new[] { 1, 3, 5 }
+        },5).Returns(true).SetName("03");
+    }
+}
