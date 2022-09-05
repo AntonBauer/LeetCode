0947ec303d2bf7fc0a4d1b0a031cc4a371f2c68b
Anton Bauer <anton.bauer@finanzen.net>
2022-09-05 08:54:29 +0200

    2. Two sum WIP

--- AddTwoNumbers/Solution.cs
+++ AddTwoNumbers/Solution.cs
@@ -17,6 +17,44 @@ internal static class Solution
 {
     public static ListNode AddTwoNumbers(ListNode l1, ListNode l2)
     {
-        return default;
+        var hasOverflow = false;
+        ListNode sumHead = default;
+        var sumTail = sumHead;
+        var first = l1;
+        var second = l2;
+
+        do
+        {
+            var s1 = first?.val ?? 0;
+            var s2 = second?.val ?? 0;
+
+            var sum = hasOverflow
+                ? s1 + s2 + 1
+                : s1 + s2;
+            var corrected = sum > 9
+                ? sum - 10
+                : sum;
+
+            hasOverflow = sum > 9;
+
+            if (sumHead == default)
+            {
+                sumHead = new ListNode(corrected);
+                sumTail = sumHead;
+            }
+            else
+            {
+                sumTail.next = new ListNode(corrected);
+                sumTail = sumTail.next;
+            }
+
+            first = first?.next;
+            second = second?.next;
+        } while (first != null && second != null);
+
+        if (hasOverflow)
+            sumTail.next = new ListNode(1);
+        
+        return sumHead;
     }
 }
--- AddTwoNumbers/Tests.cs
+++ AddTwoNumbers/Tests.cs
@@ -1,6 +1,18 @@
-﻿namespace AddTwoNumbers;
+﻿using NUnit.Framework;
 
+namespace AddTwoNumbers;
+
+[TestFixture]
 public class Tests
 {
-    
+    [Test, TestCaseSource(nameof(TestCases))]
+    public int[] TestAddTwoNumbers(int[] first, int[] second) =>
+        Solution.AddTwoNumbers(first.ToListNode(), second.ToListNode()).ToArray();
+
+    public static IEnumerable<TestCaseData> TestCases()
+    {
+        yield return new TestCaseData(new[] { 2, 4, 3 }, new[] { 5, 6, 4 }).Returns(new[] { 7, 0, 8 }).SetName("01");
+        yield return new TestCaseData(new[] { 0 }, new[] { 0 }).Returns(new[] { 0 }).SetName("02");
+        yield return new TestCaseData(new[] { 9,9,9,9,9,9,9 }, new[] { 9,9,9,9 }).Returns(new[] { 8,9,9,9,0,0,0,1 }).SetName("03");
+    }
 }
