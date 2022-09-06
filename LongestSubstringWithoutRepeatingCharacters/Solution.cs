43c2dd68ebf6a9851299e2b228cfa3bd4ed6477c
Anton Bauer <anton.bauer@finanzen.net>
2022-09-06 12:11:47 +0200

    Add #3

---
+++ LongestSubstringWithoutRepeatingCharacters/Solution.cs
@@ -0,0 +1,36 @@
+﻿namespace LongestSubstringWithoutRepeatingCharacters;
+
+// 3. Longest Substring Without Repeating Characters
+internal static class Solution
+{
+    public static int LengthOfLongestSubstring(string s) =>
+        s.Length switch
+        {
+            0 => 0,
+            1 => 1,
+            _ => CalcLongestSubstring(s)
+        };
+
+    private static int CalcLongestSubstring(string s)
+    {
+        var charsMap = new Dictionary<char, int>();
+        var length = 0;
+
+        var firstPointer = 0;
+        var secondPointer = 0;
+
+        while (secondPointer < s.Length)
+        {
+            if (!charsMap.TryAdd(s[secondPointer], secondPointer))
+            {
+                firstPointer = charsMap[s[firstPointer]] + 1;
+                charsMap[s[secondPointer]] = secondPointer;
+            }
+
+            length = Math.Max(length, secondPointer - firstPointer + 1);
+            ++secondPointer;
+        }
+        
+        return length;
+    }
+}
---
+++ LongestSubstringWithoutRepeatingCharacters/Tests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace LongestSubstringWithoutRepeatingCharacters;
+
+[TestFixture]
+public class Tests
+{
+    [Test, TestCaseSource(nameof(TestCases))]
+    public int TestLengthOfLongestSubstring(string s) => Solution.LengthOfLongestSubstring(s);
+    
+    public static IEnumerable<TestCaseData> TestCases()
+    {
+        yield return new TestCaseData("abcabcbb").Returns(3).SetName("01");
+        yield return new TestCaseData("bbbbb").Returns(1).SetName("02");
+        yield return new TestCaseData("pwwkew").Returns(3).SetName("03");
+    }
+}
