f2c47e9ff17c22da613f48eeaaca0d540850f879
Anton Bauer <anton.bauer2@generic.de>
2025-02-18 09:18:03 +0100

    implement palindrom number

--- golang/easy/09-palindrome-number/palindromeNumber.go
+++ golang/easy/09-palindrome-number/palindromeNumber.go
@@ -1,5 +1,18 @@
 package palindromeNumber
 
 func isPalindrome(x int) bool {
-	return false
+	if x < 0 {
+		return false
+	}
+
+	var reversed = 0
+	var original = x
+
+	for x > 0 {
+		var mod = x % 10
+		reversed = reversed*10 + mod
+		x = x / 10
+	}
+
+	return reversed == original
 }
