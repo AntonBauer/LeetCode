811144efc7ddc43fc0436b20f15fa189f65ece36
Anton Bauer <anton.bauer2@generic.de>
2025-02-21 15:00:20 +0100

    implement 27 Remove element solution

--- golang/easy/27-remove-element/removeElement.go
+++ golang/easy/27-remove-element/removeElement.go
@@ -2,19 +2,24 @@ package removeElement
 
 func removeElement(nums []int, val int) int {
 	if val > 50 {
-		return 0
+		return len(nums)
 	}
 
 	var delta = 0
 	var i = 0
 
-	// set item in current index to currentIndex + delta
-	// Incremet delta every time val is encountered
 	for i+delta < len(nums) {
-		if nums[i] == val {
-			delta = delta + 1
+		for i+delta < len(nums) && nums[i+delta] == val {
+			delta += 1
 		}
+
+		if i+delta >= len(nums) {
+			break
+		}
+
+		nums[i] = nums[i+delta]
+		i += 1
 	}
 
-	return i + 1
+	return i
 }
