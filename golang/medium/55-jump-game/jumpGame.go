c7e18333870777843188083c601954033b5c3ce9
Anton Bauer <anton.bauer2@generic.de>
2025-02-21 15:40:02 +0100

    implement 55 jump game solution

--- golang/medium/55-jump-game/jumpGame.go
+++ golang/medium/55-jump-game/jumpGame.go
@@ -1,5 +1,31 @@
 package jumpGame
 
 func canJump(nums []int) bool {
+	if len(nums) == 1 {
+		return true
+	}
+
+	return canJumpFrom(0, nums)
+}
+
+func canJumpFrom(currentPosition int, nums []int) bool {
+	if currentPosition == len(nums)-1 {
+		return true
+	}
+
+	if nums[currentPosition] == 0 {
+		return false
+	}
+
+	if currentPosition+nums[currentPosition] >= len(nums)-1 {
+		return true
+	}
+
+	for i := currentPosition + nums[currentPosition]; i > 0; i -= 1 {
+		if canJumpFrom(i, nums) {
+			return true
+		}
+	}
+
 	return false
 }
--- golang/medium/55-jump-game/jumpGame_test.go
+++ golang/medium/55-jump-game/jumpGame_test.go
@@ -11,6 +11,7 @@ type JumpTestCase struct {
 var testCases = []JumpTestCase{
 	{"[2,3,1,1,4]", []int{2, 3, 1, 1, 4}, true},
 	{"[3,2,1,0,4]", []int{3, 2, 1, 0, 4}, false},
+	{"[[2,5,0,0]", []int{2, 5, 0, 0}, false},
 }
 
 func TestJumpGame(t *testing.T) {
